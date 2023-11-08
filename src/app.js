a542ec91b00f350d6ac5c67335e5aee176cdba2a
unknown <mayuncaldetrini@gmail.com>
2023-11-07 21:27:10 -0300

    proyecto mercadoliebre

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ const app = express();
 
 const path = require('path');
 
+const port = process.env.PORT || 3001;
+
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-app.listen(3000, () => console.log('Servidor corriendo'));
+app.listen(port,()=>console.log(`Servidor corriendo en el puerto ${port}`));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
