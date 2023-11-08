19a67c631ef6f7c4bf343a9bac319641437f1a2d
unknown <mayuncaldetrini@gmail.com>
2023-11-07 21:09:22 -0300

    mercadoliebre

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+const path = require('path');
+
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+app.listen(3000, () => console.log('Servidor corriendo'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+})
+
+app.get('/login', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+});
+
+app.get('/registro', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/registro.html'))
+});
